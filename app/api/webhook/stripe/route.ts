785cd40c0059569061d3a12eea5b3503d1e9e809
Johnny Yu <johnnyyu601@gmail.com>
2024-04-07 23:17:50 +0800

    fix

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,23 +1,18 @@
 import stripe from "stripe";
 import { NextResponse } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
+import { headers } from "next/headers";
 
 export async function POST(request: Request) {
-  const body = await request.json();
+  const body = await request.text();
 
-  const sig = request.headers.get("stripe-signature") as string;
+  const sig = headers().get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-
-  console.log(body, sig, endpointSecret);
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(
-      JSON.stringify(body),
-      sig,
-      endpointSecret
-    );
+    event = stripe.webhooks.constructEvent(body, sig, endpointSecret);
   } catch (err) {
     return NextResponse.json({ message: "Webhook error", error: err });
   }
