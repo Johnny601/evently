7169ce95e2669c1f5f90798a35c821db2868ac1a
Johnny Yu <johnnyyu601@gmail.com>
2024-04-07 23:29:15 +0800

    fix

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -1,16 +1,11 @@
-import Stripe from "stripe";
+import stripe from "stripe";
 import { NextResponse } from "next/server";
 import { createOrder } from "@/lib/actions/order.actions";
-import { headers } from "next/headers";
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-});
 
 export async function POST(request: Request) {
   const body = await request.text();
 
-  const sig = headers().get("stripe-signature") as string;
+  const sig = request.headers.get("stripe-signature") as string;
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!;
 
   let event;
